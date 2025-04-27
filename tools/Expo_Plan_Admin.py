7ac74c0a1ca2c1d37ccef293f4e531fd49e466e7
Ajax-XIE <ajax351x@163.com>
2025-04-27 17:15:09 +0800

    adjust token

--- tools/Expo_Plan_Admin.py
+++ tools/Expo_Plan_Admin.py
@@ -132,7 +132,7 @@ def df_to_git(dataframe, file_path):
     repo_owner = "Ajax-XIE"
     repo_name = "Web_for_Expo"
     branch = "main"
-    token = st.secrets["GITHUB_TOKEN"]
+    token = st.secrets.get("GITHUB_TOKEN")
 
     if not token:
         st.write("Missing token")
