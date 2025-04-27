b38b9fc37cccffd0a32d1029814377d4c97292d4
Ajax-XIE <ajax351x@163.com>
2025-04-27 12:06:27 +0800

    change accesscontrol

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-expo_activity_change = pd.read_excel("Expo_Plan.xlsx")
-
-
-print([expo_activity_change['Date'][i] + expo_activity_change['Expo'][i] for i in list(range(len(expo_activity_change)))])
